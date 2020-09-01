df26e6df141ae9042e3613a5fa5f9513a7bc7c87
aiko-ji <68524938+aiko-ji@users.noreply.github.com>
2020-09-01 14:25:06 +0900

    編集２

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -16,6 +16,7 @@
     console.log(card);
 
     Payjp.createToken(card, (status, response) => {
+      console.log(response)
       if (status === 200) {
         const token = response.id;
         const renderDom = document.getElementById("charge-form");
